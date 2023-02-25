961474f73ad02cc6f53ba6d6774c20dea577dead
Tyron Kanamugire <tyronkanamugire@wifi-wpa-cw2-140-193-98-200.wifi.umanitoba.ca>
2023-02-25 16:12:49 -0600

    Added collisions

---
+++ collisionsT.py
@@ -0,0 +1,15 @@
+import pygame
+walls=[]
+def __init__(self):
+        self.rect = pygame.Rect(32, 32, 16, 16)
+def collision(self, dx, dy):
+        for wall in walls:
+            if self.rect.colliderect(wall.rect):
+                if dx > 0:
+                    self.rect.right = wall.rect.left
+                if dx < 0:
+                    self.rect.left = wall.rect.right
+                if dy > 0:
+                    self.rect.bottom = wall.rect.top
+                if dy < 0:
+                    self.rect.top = wall.rect.bottom
